dee69498cf6b1d8089dd7f5f36bbdbfc0c06e8de
JieYinStat <yjabcdhot@hotmail.com>
2024-03-21 17:53:38 +0800

    Test use_article() and github actions

--- tests/testthat/test-IES.R
+++ tests/testthat/test-IES.R
@@ -12,3 +12,10 @@ test_that("IES get the same result with R and RcppArmadillo", {
   q <- 16
   expect_equal(r_IES_compare(X, n, q), c_IES_compare(X, n, q))
 })
+test_that("IES R-version works", {
+  data <- data_numeric_regression
+  X <- data[-which(names(data) == "y")]
+  n <- 1000
+  q <- 16
+  expect_true(length(r_IES(X, n, q)) == 1000)
+})
