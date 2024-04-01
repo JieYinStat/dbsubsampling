0d2e693b13436b78fd017e74b6f404f3feb8e54a
JieYinStat <yjabcdhot@hotmail.com>
2024-04-01 15:05:44 +0800

    Add tests.

--- tests/testthat/test-IES.R
+++ tests/testthat/test-IES.R
@@ -12,6 +12,13 @@ test_that("IES with R works", {
   q <- 16
   expect_equal(r_IES(X, n, q, seed = 123), r_IES(X, n, q, seed = 123))
 })
+test_that("IES get the same result with R and RcppArmadillo", {
+  data <- data_numeric_regression
+  X <- data[-which(names(data) == "y")]
+  n <- 1000
+  q <- 16
+  expect_equal(r_IES_compare(X, n, q), c_IES_compare(X, n, q))
+})
 test_that("IES R-version works", {
   data <- data_numeric_regression
   X <- data[-which(names(data) == "y")]
