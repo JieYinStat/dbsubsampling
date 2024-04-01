f27fb949dc7ea22d8cdda41358b8766e3cc31d96
JieYinStat <yjabcdhot@hotmail.com>
2024-04-01 14:56:58 +0800

    Add github action: test-coverage. usethis::use_coverage function need the action (do not need configurate token).

--- tests/testthat/test-IES.R
+++ tests/testthat/test-IES.R
@@ -5,12 +5,12 @@ test_that("IES with RcppArmadillo works", {
   q <- 16
   expect_equal(IES(X, n, q, seed = 123), IES(X, n, q, seed = 123))
 })
-test_that("IES get the same result with R and RcppArmadillo", {
+test_that("IES with R works", {
   data <- data_numeric_regression
   X <- data[-which(names(data) == "y")]
-  n <- 1000
+  n <- 10
   q <- 16
-  expect_equal(r_IES_compare(X, n, q), c_IES_compare(X, n, q))
+  expect_equal(r_IES(X, n, q, seed = 123), r_IES(X, n, q, seed = 123))
 })
 test_that("IES R-version works", {
   data <- data_numeric_regression
